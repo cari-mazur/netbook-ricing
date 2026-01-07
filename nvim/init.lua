d81a77ba935166886f30539a1bc2788c14221a26
cari-mazur <carinamazur1@gmail.com>
2026-01-07 00:43:39 -0300

    Add theming configuration for low spec Debian

---
+++ nvim/init.lua
@@ -0,0 +1,17 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.updatetime = 200
+vim.opt.signcolumn = "yes"
